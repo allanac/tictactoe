1d796faeb1f230f989bd4458bda1018cf67dab33
allanac <acole006@fiu.edu>
2017-08-23 12:42:50 -0400

    added draw feedback

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -11,71 +11,79 @@ $(document).ready(function(){
     [' ', ' ',' ']
   ];
 
+  newGame(grid);
+
+
   $('.col').click(function(){
 
     $(this).html(playerKey);
     row = $(this).attr('id').charAt(0);
     col = $(this).attr('id').charAt(1);
 
-      grid[row][col] = 'X';
+      if (grid[row][col] !== ' '){
+        alert('Space Taken');
+      }
+      else {
+        grid[row][col] = 'X';
+      }
 
-// Horizontal Wins
+  var playerWin = playerMove(grid);
+  var computerWin = computerMove (grid);
 
-    if (grid[0][0] == 'X' && grid[0][1] == 'X' && grid [0][2] == 'X')
-        {
-        alert('X Wins!');
-        return;
-        }
-    else if (grid[1][0] == 'X' && grid[1][1] == 'X' && grid [1][2] == 'X')
-        {
-          alert('X Wins!');
-          return;
-        }
-    else if (grid[2][0] == 'X' && grid[2][1] == 'X' && grid [2][2] == 'X')
-        {
-      alert('X Wins!');
-      return;
-        }
-
-
-// Vertical Wins
-
-    if (grid[0][0] == 'X' && grid[1][0] == 'X' && grid [2][0] == 'X')
-        {
-        alert('X Wins!');
-        return;
-        }
-    else if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid [2][1] == 'X')
-        {
-          alert('X Wins!');
-          return;
-        }
-    else if (grid[0][2] == 'X' && grid[1][2] == 'X' && grid [2][2] == 'X')
-        {
-      alert('X Wins!');
-      return;
-        }
-
-// Diagonal Wins
-    if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid [2][2] == 'X')
-        {
-        alert('X Wins!');
-        return;
-        }
-    else if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid [2][0] == 'X')
-        {
-          alert('X Wins!');
-          return;
-        }
-
-  // call AI Function
-
-  computerMove (grid);
+  //no win
+  if (playerWin !== true && computerWin !== true){
+    checkDraw(grid);
+  }
 
   });
+});
+
+function newGame(grid) {
+
+
+  $('.btn').click(function(){
+    $('.col').html(' ');
+  for (i = 0; i < grid.length; i++) {
+   for (j = 0; j < grid.length; j++) {
+      {
+        grid[i][j] = " ";
+        }
+      }
+    }
 
 });
 
+};
+
+function checkDraw(grid){
+
+  var emptySpaces = [];
+
+    for (i = 0; i < grid.length; i++) {
+      for (j = 0; j < grid.length; j++) {
+        if (grid[i][j] == " ") {
+          emptySpaces.push({i,j});
+        }
+        }
+      }
+    if (emptySpaces.length === 0){
+      alert('Draw!');
+    }
+};
+
+function keepScore(computerMove,playerMove){
+
+  var playerScore = 0;
+  var computerScore = 0;
+
+  if (computerMove === true){
+    computerScore ++;
+  }
+  else if (playerMove) {
+    playerScore ++;
+  }
+
+};
 
 function computerMove(grid){
 
@@ -90,14 +98,128 @@ function computerMove(grid){
     }
     var openSpot = emptySpaces[Math.floor(Math.random()* emptySpaces.length)];
 
+    if (openSpot === undefined) {
+      return;
+    }
+
     $('#'+ openSpot.i + openSpot.j).html(computerKey);
 
     grid[openSpot.i][openSpot.j] = computerKey;
 
     console.log(openSpot);
+
+
+    // Horizontal Wins
+
+        if (grid[0][0] == 'O' && grid[0][1] == 'O' && grid [0][2] == 'O')
+            {
+            alert('0 Wins!');
+            return true;
+            }
+
+        else if (grid[1][0] == 'O' && grid[1][1] == 'O' && grid [1][2] == 'O')
+                {
+                  alert('O Wins!');
+                  return true;
+                }
+
+        else if (grid[2][0] == 'O' && grid[2][1] == 'O' && grid [2][2] == 'O')
+            {
+            alert('O Wins!');
+            return true;
+            }
+
+    // Vertical Wins
+
+        else if (grid[0][0] == 'O' && grid[1][0] == 'O' && grid [2][0] == 'O')
+            {
+              alert('O Wins!');
+              return  true;
+            }
+
+        else if (grid[0][1] == 'O' && grid[1][1] == 'O' && grid [2][1] == 'O')
+            {
+              alert('O Wins!');
+              return true;
+            }
+
+        else if (grid[0][2] == 'O' && grid[1][2] == 'O' && grid [2][2] == 'O')
+            {
+              alert('O Wins!');
+              return true;
+            }
+
+    // Diagonal Wins
+
+
+       if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid [2][2] == 'O')
+            {
+              alert('O Wins!');
+              return true;
+            }
+
+        else if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid [2][0] == 'O')
+            {
+              alert('O Wins!');
+              return true;
+            }
+
 };
 
-function gameOver (){
 
+function playerMove(grid){
+
+  // Horizontal Wins
+
+      if (grid[0][0] == 'X' && grid[0][1] == 'X' && grid [0][2] == 'X')
+          {
+          alert('X Wins!');
+          return true;
+          }
+
+      else if (grid[1][0] == 'X' && grid[1][1] == 'X' && grid [1][2] == 'X')
+          {
+            alert('X Wins!');
+            return true;
+          }
+
+      else if (grid[2][0] == 'X' && grid[2][1] == 'X' && grid [2][2] == 'X')
+          {
+        alert('X Wins!');
+        return true;
+          }
+
+
+  // Vertical Wins
+
+      else if (grid[0][0] == 'X' && grid[1][0] == 'X' && grid [2][0] == 'X')
+          {
+          alert('X Wins!');
+          return true;
+          }
+
+      else if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid [2][1] == 'X')
+          {
+            alert('X Wins!');
+            return true;
+          }
+      else if (grid[0][2] == 'X' && grid[1][2] == 'X' && grid [2][2] == 'X')
+          {
+        alert('X Wins!');
+        return true;
+          }
+
+
+  // Diagonal Wins
+    else if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid [2][2] == 'X')
+          {
+          alert('X Wins!');
+          return true;
+          }
+      else if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid [2][0] == 'X')
+          {
+            alert('X Wins!');
+            return true;
+          }
 
 };
