f0a1c926c062c6e5f40ba7946f8dc4bcde05c696
allanac <acole006@fiu.edu>
2017-08-25 12:45:32 -0400

    added sounds, fixed click bug where computer kept moving, added click sounds, and feedback sounds

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,6 +2,7 @@ var playerKey = 'X';
 var computerKey = 'O';
 var row = 0;
 var col = 0;
+var blockClicks = false;
 
 $(document).ready(function(){
 
@@ -13,19 +14,24 @@ $(document).ready(function(){
 
   newGame(grid);
 
-
   $('.col').click(function(){
 
-    $(this).html(playerKey);
+    play();
+
+    if (blockClicks === true) {
+      return;
+    }
+
     row = $(this).attr('id').charAt(0);
     col = $(this).attr('id').charAt(1);
 
-      if (grid[row][col] !== ' '){
-        alert('Space Taken');
-      }
-      else {
-        grid[row][col] = 'X';
-      }
+    if (grid[row][col] !== ' '){
+      alert('Space Taken');
+      return;
+    }
+
+    $(this).html(playerKey);
+    grid[row][col] = 'X';
 
   var playerWin = playerMove(grid);
   var computerWin = computerMove (grid);
@@ -35,16 +41,19 @@ $(document).ready(function(){
     checkDraw(grid);
   }
   else if (playerWin === true){
+    blockClicks = true;
     $('.win-feedback').addClass('win-play');
+    playWin ();
   }
   else if (computerWin === true){
+    blockClicks = true;
     $('.lose-feedback').addClass('lose-play');
+    playLose();
   }
   });
 });
 
 function newGame(grid) {
-
 
   $('.btn').click(function(){
     $('.col').html(' ');
@@ -54,11 +63,13 @@ function newGame(grid) {
       }
     }
 
-    $('.win-feedback').removeClass('win-play');
-    $('.lose-feedback').removeClass('lose-play');
-    $('.draw-feedback').removeClass('draw-play');
+  $('.win-feedback').removeClass('win-play');
+  $('.lose-feedback').removeClass('lose-play');
+  $('.draw-feedback').removeClass('draw-play');
 
-});
+  blockClicks = false;
+
+  });
 
 };
 
@@ -66,17 +77,18 @@ function checkDraw(grid){
 
   var emptySpaces = [];
 
-    for (i = 0; i < grid.length; i++) {
-      for (j = 0; j < grid.length; j++) {
-        if (grid[i][j] == " ") {
-          emptySpaces.push({i,j});
-        }
-        }
+  for (i = 0; i < grid.length; i++){
+    for (j = 0; j < grid.length; j++){
+      if (grid[i][j] == " "){
+        emptySpaces.push({i,j});
       }
-    if (emptySpaces.length === 0){
-      alert('Draw!');
-      $('.draw-feedback').addClass('draw-play');
     }
+  }
+  if (emptySpaces.length === 0){
+    $('.draw-feedback').addClass('draw-play');
+    blockClicks = true;
+    playDraw();
+  }
 };
 
 function keepScore(computerMove,playerMove){
@@ -84,10 +96,12 @@ function keepScore(computerMove,playerMove){
   var playerScore = 0;
   var computerScore = 0;
 
-  if (computerMove === true){
+  if (computerMove === true)
+  {
     computerScore ++;
   }
-  else if (playerMove) {
+  else if (playerMove)
+  {
     playerScore ++;
   }
 
@@ -104,137 +118,135 @@ function computerMove(grid){
         }
       }
     }
-    var openSpot = emptySpaces[Math.floor(Math.random()* emptySpaces.length)];
 
-    if (openSpot === undefined) {
-      return;
-    }
+  var openSpot = emptySpaces[Math.floor(Math.random()* emptySpaces.length)];
 
-    $('#'+ openSpot.i + openSpot.j).html(computerKey);
+  if (openSpot === undefined)
+  {
+    return;
+  }
 
-    grid[openSpot.i][openSpot.j] = computerKey;
+  $('#'+ openSpot.i + openSpot.j).html(computerKey);
 
-    console.log(openSpot);
+  grid[openSpot.i][openSpot.j] = computerKey;
+
+  console.log(openSpot);
 
 
     // Horizontal Wins
 
-        if (grid[0][0] == 'O' && grid[0][1] == 'O' && grid [0][2] == 'O')
-            {
-            alert('0 Wins!');
-            return true;
-            }
+  if (grid[0][0] == 'O' && grid[0][1] == 'O' && grid [0][2] == 'O')
+      {
+      return true;
+      }
 
-        else if (grid[1][0] == 'O' && grid[1][1] == 'O' && grid [1][2] == 'O')
-                {
-                  alert('O Wins!');
-                  return true;
-                }
+  else if (grid[1][0] == 'O' && grid[1][1] == 'O' && grid [1][2] == 'O')
+      {
+        return true;
+      }
 
-        else if (grid[2][0] == 'O' && grid[2][1] == 'O' && grid [2][2] == 'O')
-            {
-            alert('O Wins!');
-            return true;
-            }
+  else if (grid[2][0] == 'O' && grid[2][1] == 'O' && grid [2][2] == 'O')
+      {
+        return true;
+      }
 
     // Vertical Wins
 
-        else if (grid[0][0] == 'O' && grid[1][0] == 'O' && grid [2][0] == 'O')
-            {
-              alert('O Wins!');
-              return  true;
-            }
+  else if (grid[0][0] == 'O' && grid[1][0] == 'O' && grid [2][0] == 'O')
+      {
+        return  true;
+      }
 
-        else if (grid[0][1] == 'O' && grid[1][1] == 'O' && grid [2][1] == 'O')
-            {
-              alert('O Wins!');
-              return true;
-            }
+  else if (grid[0][1] == 'O' && grid[1][1] == 'O' && grid [2][1] == 'O')
+      {
+        return true;
+      }
 
-        else if (grid[0][2] == 'O' && grid[1][2] == 'O' && grid [2][2] == 'O')
-            {
-              alert('O Wins!');
-              return true;
-            }
+  else if (grid[0][2] == 'O' && grid[1][2] == 'O' && grid [2][2] == 'O')
+      {
+        return true;
+      }
 
     // Diagonal Wins
 
 
-       if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid [2][2] == 'O')
-            {
-              alert('O Wins!');
-              return true;
-            }
+  if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid [2][2] == 'O')
+      {
+        return true;
+      }
 
-        else if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid [2][0] == 'O')
-            {
-              alert('O Wins!');
-              return true;
-            }
+  else if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid [2][0] == 'O')
+      {
+        return true;
+      }
 
 };
 
-
 function playerMove(grid){
-
   // Horizontal Wins
+  if (grid[0][0] == 'X' && grid[0][1] == 'X' && grid [0][2] == 'X')
+    {
+      return true;
+    }
 
-      if (grid[0][0] == 'X' && grid[0][1] == 'X' && grid [0][2] == 'X')
-          {
-          alert('X Wins!');
+  else if (grid[1][0] == 'X' && grid[1][1] == 'X' && grid [1][2] == 'X')
+    {
+      return true;
+    }
 
-          return true;
-          }
-
-      else if (grid[1][0] == 'X' && grid[1][1] == 'X' && grid [1][2] == 'X')
-          {
-            alert('X Wins!');
-
-            return true;
-          }
-
-      else if (grid[2][0] == 'X' && grid[2][1] == 'X' && grid [2][2] == 'X')
-          {
-        alert('X Wins!');
-        return true;
-          }
-
+  else if (grid[2][0] == 'X' && grid[2][1] == 'X' && grid [2][2] == 'X')
+    {
+      return true;
+    }
 
   // Vertical Wins
 
-      else if (grid[0][0] == 'X' && grid[1][0] == 'X' && grid [2][0] == 'X')
-          {
-          alert('X Wins!');
+  else if (grid[0][0] == 'X' && grid[1][0] == 'X' && grid [2][0] == 'X')
+    {
+      return true;
+    }
 
-          return true;
-          }
-
-      else if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid [2][1] == 'X')
-          {
-            alert('X Wins!');
-
-            return true;
-          }
-      else if (grid[0][2] == 'X' && grid[1][2] == 'X' && grid [2][2] == 'X')
-          {
-        alert('X Wins!');
-
-        return true;
-          }
-
+  else if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid [2][1] == 'X')
+    {
+      return true;
+    }
+  else if (grid[0][2] == 'X' && grid[1][2] == 'X' && grid [2][2] == 'X')
+    {
+      return true;
+    }
 
   // Diagonal Wins
-    else if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid [2][2] == 'X')
-          {
-          alert('X Wins!');
+  else if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid [2][2] == 'X')
+    {
+      return true;
+    }
+  else if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid [2][0] == 'X')
+    {
+      return true;
+    }
 
-          return true;
-          }
-      else if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid [2][0] == 'X')
-          {
-            alert('X Wins!');
+};
 
-            return true;
-          }
+function play()
+{
+    var audio = document.getElementById("clickAudio");
+    audio.play();
+};
 
+function playWin()
+{
+    var audio = document.getElementById("winAudio");
+    audio.play();
+};
+
+function playLose()
+{
+    var audio = document.getElementById("loseAudio");
+    audio.play();
+};
+
+function playDraw()
+{
+    var audio = document.getElementById("drawAudio");
+    audio.play();
 };
